bd03d4689176eb326c16f83445fa259a7365ded0
Nathan <urwin.nathan@gmail.com>
2024-07-07 17:00:58 +0100

    create test run for todo item funcs

--- src/index.js
+++ src/index.js
@@ -7,10 +7,18 @@ import setTodoComplete from "./setTodoComplete.js";
 import home from "./home.js";
 import projects from "./projects.js";
 
+
+
+/* DOM-Related Stuff */
+
+/* Tests */
 function testLog() {
 	console.log("Successful Link");
 }
 
 testLog();
 
-/* DOM-Related Stuff */
+const testTodoItem = createTodo("Iron Laundry", "Iron the rest of the laundry leftover from yesterday", "24/07/2024", 1, 1);
+console.log(testTodoItem);
+changeTodoPriority(testTodoItem, 2);
+console.log(testTodoItem);
