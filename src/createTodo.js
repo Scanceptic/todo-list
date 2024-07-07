d59313697c17acfd7dc3579ee0136f7019fd0607
Nathan <urwin.nathan@gmail.com>
2024-07-07 16:53:40 +0100

    make initial createTodo function

--- src/createTodo.js
+++ src/createTodo.js
@@ -1,3 +1,5 @@
+import { todo } from "node:test";
+
 /* Creates a Todo list item based user inputs */
 export default function createTodo(
 	title,
@@ -10,6 +12,15 @@ export default function createTodo(
 	// take input values provided for the todo item
 	// if values not provided for a parameter, use a default value instead
 	// create JSON formatted object that can be stored in local storage
-	// store it in local storage under the inputted project parent
+	const todoItem = {
+		title,
+		description,
+		dueDate,
+		priority,
+		project,
+	};
+	// return completed item
+	return todoItem;
+	// another module can store it in local storage under the inputted project parent
 	// no DOM necessary as only storage needs to be changed
 }
