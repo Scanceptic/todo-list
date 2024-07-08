0b552df7e46533805ac6e146621dae3bd502ced5
Nathan <urwin.nathan@gmail.com>
2024-07-08 11:16:39 +0100

    make sidebar updater func

---
+++ src/updateSidebar.js
@@ -0,0 +1,28 @@
+export default function updateSidebar() {
+	// get sidebar DOM
+	const sidebar = document.getElementById("sidebar");
+	// clear sidebar DOM
+	while (sidebar.firstChild) {
+		sidebar.remove(sidebar.firstChild);
+	}
+	// loop through projects in storage
+	for (let i = 0; i < localStorage.length; i++) {
+		// get project name for each position
+		const projectName = localStorage.key();
+		// create button with textContent = projectname
+		projectBtn = document.createElement("button");
+		projectBtn.textContent = projectName;
+		// add eventlistener to button
+		projectBtn.addEventListener("click", () => {
+			// TODO script event listener
+			console.log("Project button clicked: " + projectName);
+		});
+		// append button to sidebar
+		sidebar.appendChild(projectBtn);
+	}
+
+	// update sidebar with projects in storage
+	// each project should be a button element
+	// on click get local storage for project and load it onto DOM (under content)
+	// clear content before appending new project tasks
+}
