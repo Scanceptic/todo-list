fbea39b6f2df21f1ab3cd5c5fc88995db6275ca2
Nathan <urwin.nathan@gmail.com>
2024-07-08 16:18:57 +0100

    set out basic functions

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
-/* Creating new tasks */
+/* Creating new task objects */
 // factory function
-function createTask (title, description, dueDate, priority) {
-
+function createTask(title, description, dueDate, priority, project) {
+	const taskObject = { title, description, dueDate, priority, project, completed: false };
+	return taskObject;
 }
 /* Setting tasks as complete */
-
+function toggleComplete(task) {
+    task.completed = !task.completed;
+    return taskObject;
+}
 /* Editing a task */
+function editTask() {
+    
+}
