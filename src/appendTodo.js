293e8ad6f4d02a3f3a1069483c447a4fef9ae0f0
Nathan <urwin.nathan@gmail.com>
2024-07-08 15:38:18 +0100

    start new branch to redesign

--- src/appendTodo.js
+++
@@ -1,9 +0,0 @@
-// takes in formatted task element ready for DOM appending
-export default function appendTodo(task) {
-	try {
-		const tasklist = document.getElementById("tasks");
-		tasklist.appendChild(task);
-	} catch (error) {
-		console.log(error);
-	}
-}
--- src/clearStorage.js
+++
@@ -1,8 +0,0 @@
-/* DELETES ALL LOCALSTORAGE CONTENTS - FOR TESTING ONLY */
-export default function clearStorage() {
-	try {
-		localStorage.clear();
-	} catch (error) {
-		console.log(error);
-	}
-}
