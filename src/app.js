b40b0a3f5ea3fd80d47ee9707890a523e1e6bc95
Nathan <urwin.nathan@gmail.com>
2024-07-09 09:29:43 +0100

    outline basic pseudocode for app logic

--- src/app.js
+++ src/app.js
@@ -1,8 +1,38 @@
 // Everything in app.js should work with pure JS objects - no HTML, no localStorage
+// Make an empty task array on pageload?
+// fill with stuff from localStorage if there is any
+function createTaskArray() {
+	const taskArray = [];
+	return taskArray;
+}
 
 /* Creating new task objects */
-// factory function
+// factory function for creating new task objects
+// creating a task adds it to the session taskArray, and returns the taskObject as well
 function createTask(title, description, dueDate, priority, project) {
+	// check for acceptable parameters types and formats
+	// title, description and projectname can be anything since they are stored as strings
+
+	// priority must be number above 0
+	if (typeof priority !== "number" || priority < 1) {
+		console.log("Priority was not a number or was less than 1");
+		return null;
+	}
+
+	// dueDate must be valid date
+	// try data parsing with date module, catch return null and log error and console.log("Invalid date")
+
+	// check title is unique
+	// loop through current array of tasks
+	for (let i = 0; i < taskArray.length; i++) {
+		// if task.title === title parameter
+		if (taskArray[i].title === title) {
+			// cancel task creation and tell user
+			console.log("Task title is not unique");
+			return null;
+		}
+	}
+
 	const taskObject = {
 		title,
 		description,
@@ -11,16 +41,9 @@ function createTask(title, description, dueDate, priority, project) {
 		project,
 		completed: false,
 	};
-	return taskObject;
-}
 
-/* Create new project */
-function createProject(name, colour) {
-	const project = {
-		name,
-		colour,
-	};
-	return project;
+	taskArray.push(taskObject);
+	return taskObject;
 }
 
 /* Setting tasks as complete */
@@ -30,6 +53,3 @@ function toggleComplete(task) {
 }
 /* Editing a task */
 function editTask() {}
-
-/* Load from localStorage on page load */
-function loadStorage() {}
